cbda0398b4db20f95ff9ed94aefc9370149460a9
saurabh kumar pandey <80593478+Aagd12@users.noreply.github.com>
2021-08-16 11:18:51 +0530

    second

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const time = Math.random() + 1.5; // I’m adding .5 here so the flicker isn’t too obnoxious
-const left = Math.random() + 2; // I’m adding .5 here so the flicker isn’t too obnoxious
+const time1 = Math.random() + 2; // I’m adding .5 here so the flicker isn’t too obnoxious
 
 const red = document.querySelector('.duck1');
 const red1 = document.querySelector('.duck3');
 
 red.style.setProperty('--animation-time', time + 's');
-red1.style.setProperty('--animation-time', time + 's');
+red1.style.setProperty('--animation-time', time1 + 's');
